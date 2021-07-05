678d6febe262fe01711e384de9d57fd900d305ff
sank2000 <santhoshvelr@yahoo.com>
2021-07-05 19:25:41 +0530

    added backend

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const socketIo = require('socket.io');
 
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080, () => {
+const expressServer = app.listen(8080, () => {
   console.log('server running at port 8080');
+});
+
+const io = socketIo(expressServer);
+
+io.on('connection', (socket) => {
+  socket.emit('messageFromServer', { data: 'Welcome to the socketIo server' });
+
+  socket.on('newMessageToServer', (msg) => {
+    console.log(msg);
+    io.emit('messageToClients', { text: msg.text });
+  });
 });
